5ddfadfb9362e72df9cecbf047cefa01b5b9923f
sunilk747 <sunilk747@gmail.com>
2018-06-14 14:46:48 +0530

    Updated phantom.js download url

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo yum -y install gcc gcc-c++ make flex bison gperf ruby \
   libpng-devel libjpeg-devel git nodejs npm fontconfig freetype freetype-devel fontconfig-devel libstdc++
 
 rm -rf /tmp/phantomjs.tar.bz2
-wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2 -O /tmp/phantomjs.tar.bz2
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2 -O /tmp/phantomjs.tar.bz2
 mkdir -p phantomjs
 tar -xjvf /tmp/phantomjs.tar.bz2 -C phantomjs --strip-components 1
 ./phantomjs/bin/phantomjs /phantomjs/examples/hello.js
